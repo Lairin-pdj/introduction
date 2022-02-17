8186fe3022bb2e94665f25a5669f8daa8ee53e5c
Dong-jun Park <70496139+Lairin-pdj@users.noreply.github.com>
2022-02-17 15:54:40 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,30 +1,32 @@
-const main = document.querySelector(".main");
-const mainheight = main.clientHeight;
-const indicator = document.querySelector(".scrolled-indicator-fill");
-const nav = document.querySelector(".nav");
-const navs = document.querySelector(".nav_items");
-document.addEventListener('scroll', function(){
-    if (scrollY != 0){
-        nav.classList.add("move_navin");
-        nav.classList.remove("move_navout");
-    }
-    else{
-        nav.classList.add("move_navout");
-        nav.classList.remove("move_navin");
-    }
-    // 네비 하이라이트 용
-    const target = parseInt((scrollY + (mainheight * 0.2)) / mainheight) * 2 + 1;
-    for (var i = 1; i < 10; i += 2){
-        if (i == target){
-            navs.childNodes[i].childNodes[0].style.color = "red";
+window.onload = function(){
+    const main = document.querySelector(".main");
+    const mainheight = main.clientHeight;
+    const indicator = document.querySelector(".scrolled-indicator-fill");
+    const nav = document.querySelector(".nav");
+    const navs = document.querySelector(".nav_items");
+    document.addEventListener('scroll', function(){
+        if (scrollY != 0){
+            nav.classList.add("move_navin");
+            nav.classList.remove("move_navout");
         }
         else{
-            navs.childNodes[i].childNodes[0].style.color = "blue";
+            nav.classList.add("move_navout");
+            nav.classList.remove("move_navin");
         }
-    }
+        // 네비 하이라이트 용
+        const target = parseInt((scrollY + (mainheight * 0.2)) / mainheight) * 2 + 1;
+        for (var i = 1; i < 10; i += 2){
+            if (i == target){
+                navs.childNodes[i].childNodes[0].style.color = "red";
+            }
+            else{
+                navs.childNodes[i].childNodes[0].style.color = "blue";
+            }
+        }
 
-    // 스크롤 체크
-    const scrollable = document.documentElement.scrollHeight - mainheight;
-    percent = Math.ceil((scrollY / scrollable) * 100);
-    indicator.style.width = `${percent}%`;
-});
+        // 스크롤 체크
+        const scrollable = document.documentElement.scrollHeight - mainheight;
+        percent = Math.ceil((scrollY / scrollable) * 100);
+        indicator.style.width = `${percent}%`;
+    });
+}
